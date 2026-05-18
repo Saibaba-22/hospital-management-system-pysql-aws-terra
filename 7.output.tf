5e550ef4d5bb89abad1fab2cd89fab519c65bfef
Saibaba Kola <146579091+Saibaba-22@users.noreply.github.com>
2026-05-18 15:14:06 +0530

    Update 7.output.tf

--- 7.output.tf
+++ 7.output.tf
@@ -38,6 +38,5 @@ output "vpc_id" {
 }
 
 output "subnet_ids" {
-  value = concat( aws_subnet.sai01_public_subnet[*].id,
-  aws_subnet.sai01_private_subnet[*].id )
+  value = ( aws_subnet.sai01_public_subnet[*].id)
 }
